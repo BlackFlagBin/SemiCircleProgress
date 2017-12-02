a5160f03a06fca470db4043ac1685af9d62ff02e
任志彬 <renzb@51weker.cn>
2017-12-02 10:48:02 +0800

    修改示例

--- app/src/main/java/com/blackflagbin/myapplication/MainActivity.kt
+++ app/src/main/java/com/blackflagbin/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.blackflagbin.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.zhidian.myapplication.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        progress.updateProgress(0f)
+        progress.updateProgress(75f)
     }
 }
